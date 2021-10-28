9a134256b2a24879cf115631ade27179c854469b
AlexVipond <alex.r.vipond@gmail.com>
2021-10-27 23:20:06 -0500

    design thumbnails

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="h-screen w-screen flex items-center justify-center bg-primary-400">
+    <section class="relative h-[720px] w-[1280px] flex pl-20 pb-20 overflow-hidden bg-primary-100">
+      <div class="text-primary-200">
+        <div class="absolute top-0 left-0 translate-x-[-80px] translate-y-[-110px] p-8 rounded-full bg-primary-300">
+          <IconVideo class="h-[380px] w-[380px]" />
+        </div>
+        <div class="absolute bottom-0 right-0 translate-x-[80px] translate-y-[80px] p-8 rounded-full bg-primary-300">
+          <IconCode class="h-[520px] w-[520px]" />
+        </div>
+      </div>
+
+      <section class="mt-auto relative w-[calc(3/5*100%)] flex flex-col gap-10">
+        <h1 class="text-7xl font-bold text-primary-1000">The Power of <br />Vue 3 Function Refs</h1>
+        <h2 class="text-4xl tracking-widest font-bold uppercase text-primary-900">Part 1</h2>
+      </section>
+
+      <div class="
+        absolute top-0 left-1/2 translate-x-[110px] translate-y-[30px]
+        h-52 w-52
+        rounded-full overflow-hidden shadow-avatar
+      ">
+        <img
+          class="h-full w-full"
+          src="/headshot-2.jpg"
+        />
+      </div>
+      <div class="
+        absolute top-0 left-1/2 -translate-x-1/2 translate-y-[55px]
+        h-80 w-80
+        flex items-center justify-center p-12
+        rounded-full overflow-hidden bg-white border-white border-[4px] shadow-avatar
+      ">
+        <img
+          class="w-full"
+          src="/vue-logo.png"
+        />
+      </div>
+    </section>
+  </main>
+</template>
+
+<script setup lang="ts">
+import IconCode from './components/IconCode.vue'
+import IconVideo from './components/IconVideo.vue'
+</script>
